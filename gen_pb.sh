2cf02bd7e81bda8608928a454a6243ec49b953cb
zhongkaizhu <zhongkaizhu@tencent.com>
2021-06-01 17:18:36 +0800

    pb: add gen_pb.sh

---
+++ gen_pb.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+list=(
+./core/stream/stream.proto
+./core/image/image.proto
+./core/log/log_pb/log.proto
+./core/os/android/apk/apk.proto
+./core/os/android/keycodes.proto
+./core/os/device/gpu_counter_descriptor.proto
+./core/os/device/device.proto
+./core/os/device/bind/bind.proto
+./core/data/pod/pod.proto
+./core/data/protoutil/testprotos/testprotos.proto
+)
+
+for i in "${list[@]}"; do
+    protoc --go_out=./ $i
+done
+
+cp -frp github.com/google/gapid/core ./
+rm -rf github.com
